1d85c51a7a08e03629e2f9dc27991ce2a5ed8040
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-10-05 13:47:15 +0200

    Add functionality to previous and next buttons

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ function changeSlide() {
   slides[currentSlide].classList.add("active");
 }
 
-setInterval(changeSlide, 3000);
+setInterval(changeSlide, 8000);
+
+function prevSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide -= 1;
+
+  if (currentSlide === -1) {
+    currentSlide = slides.length - 1;
+  }
+  slides[currentSlide].classList.add("active");
+}
+
+/* Buttons */
+
+let prevButton = document.querySelector(".prev-btn");
+let nextButton = document.querySelector(".next-btn");
+
+prevButton.addEventListener("click", () => {
+  prevSlide();
+});
+
+nextButton.addEventListener("click", () => {
+  changeSlide();
+});
