d055f8eb0dddd993a3d4b2936faa152226d94d43
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-10-05 14:09:03 +0200

    chore: Update slider to function on div instead of img

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-let slides = document.querySelectorAll(".slider-container img");
-
+let slides = document.querySelectorAll(".slider-container > div");
 let currentSlide = 0;
+
+slides[currentSlide].classList.add("active");
 
 function changeSlide() {
   slides[currentSlide].classList.remove("active");
-  currentSlide += 1;
-  if (currentSlide === slides.length) {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
     currentSlide = 0;
+  }
+  slides[currentSlide].classList.add("active");
+}
+
+function prevSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
   }
   slides[currentSlide].classList.add("active");
 }
 
 setInterval(changeSlide, 8000);
 
-function prevSlide() {
-  slides[currentSlide].classList.remove("active");
-  currentSlide -= 1;
-
-  if (currentSlide === -1) {
-    currentSlide = slides.length - 1;
-  }
-  slides[currentSlide].classList.add("active");
-}
-
-/* Buttons */
-
+// Buttons
 let prevButton = document.querySelector(".prev-btn");
 let nextButton = document.querySelector(".next-btn");
 
-prevButton.addEventListener("click", () => {
-  prevSlide();
-});
-
-nextButton.addEventListener("click", () => {
-  changeSlide();
-});
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", changeSlide);
